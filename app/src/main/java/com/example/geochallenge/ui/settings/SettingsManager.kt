4a87570fefffde9f0e5d83f679b0b650f0043aee
danbi <danbildan@gmail.com>
2020-01-04 04:02:51 +0300

    smart image records

--- app/src/main/java/com/example/geochallenge/ui/settings/SettingsManager.kt
+++ app/src/main/java/com/example/geochallenge/ui/settings/SettingsManager.kt
@@ -8,12 +8,12 @@ import javax.inject.Inject
 class SettingsManager @Inject constructor(val appDelegate: AppDelegate) {
 
     companion object {
-
         const val RUSSIAN_LANG = "1"
         const val ENGLISH_LANG = "2"
     }
 
-    fun getTaskLang(): String? {
+    fun getDefaultTaskLang(): String? {
+
         return PreferenceManager
             .getDefaultSharedPreferences(appDelegate)
             .getString(SettingsActivity.LANG_TASK_KEY, null)
